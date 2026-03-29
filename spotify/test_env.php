ca00074f268cf16d583f9e6c9bfec48ac4b86a26
online make <109430994+Nalymodrido@users.noreply.github.com>
2026-03-28 22:37:12 -0600

    Delete spotify directory

--- spotify/test_env.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("Content-Type: text/plain");
-echo "Prepend: " . ini_get("auto_prepend_file") . "\n";
-echo "Append: " . ini_get("auto_append_file") . "\n";
-echo "Self: " . $_SERVER['PHP_SELF'] . "\n";
-?>
